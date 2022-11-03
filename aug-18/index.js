9dc85f4c4d74c9e1ae716ea514041c212b5b391f
Ashish <ashishrajput9663@gamil.com>
2022-11-03 20:18:28 +0530

    Assignments

--- aug-18/index.js
+++ aug-18/index.js
@@ -1,4 +1,53 @@
-console.log("connet");
+// console.log("connet");
 
-const ele = document.querySelector("ul > li : first-child");
-console.log(ele);
+// const ele = document.querySelector("ul > li:first-child");
+// console.log(ele);
+
+// const elemt = document.querySelectorAll("ul > li:first-child");
+// console.log(elemt);
+
+// const elemts = document.querySelectorAll("ul > li");
+// console.log(elemts);
+
+// const elemtsl = document.querySelectorAll("ul:last-of-type > li");
+// console.log(elemtsl);
+
+// const classname = document.getElementsByClassName("ashu");
+// console.log(classname);
+
+// document.getElementById("ashish").classList.add("ashu");
+// console.log(classname);
+
+// const classname = document.querySelectorAll(".ashu");
+// console.log(classname);
+
+// document.getElementById("ashish").classList.add("ashu");
+// console.log(classname);
+
+// const classnam = document.querySelectorAll(".ashu");
+// console.log(classnam);
+
+// const add_btn=document.createElement("button");
+// document.body.appendChild(add_btn);
+
+// add_btn.innerText="click Me";
+
+// const classname = document.getElementsByClassName("ashu");
+// // const toPut=document.createElement("p");
+
+// // classname.appendChild(toPut);
+// const ptag=classname.createElement("p");
+// // ptag.innerText="lorem30";
+// document.body.createElement("p").innerText="lorem30";
+
+const getall=document.getElementsByClassName("ashu");
+console.log(getall);
+const ht=document.createElement("p");
+const txt=ht.appendChild("li");
+txt.innerText="kjjjjk";
+console.log(getall);
+
+
+
+
+
