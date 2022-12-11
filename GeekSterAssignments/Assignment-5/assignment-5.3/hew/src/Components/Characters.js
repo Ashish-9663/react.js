bc096326b770f8ac95d975dfb00bc876cdfc0f1c
Ashish <ashishrajput9663@gamil.com>
2022-12-11 21:04:14 +0530

    Assignments2

---
+++ GeekSterAssignments/Assignment-5/assignment-5.3/hew/src/Components/Characters.js
@@ -0,0 +1,6 @@
+
+
+export const numbersLetters = "0123456789";
+export const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+export const symbolsLetters = "`~!@#$%&*?";
--- GeekSterAssignments/Assignment-5/assignment-5.3/hew/src/Components/Generator.js
+++ GeekSterAssignments/Assignment-5/assignment-5.3/hew/src/Components/Generator.js
@@ -1,44 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { upperCaseLetters, lowerCaseLetters, symbolsLetters, numbersLetters } from './Characters';
 
-const Header = () => {
+const Generator = () => {
+  const[password, setPassword]=useState("");
+  const[passwordLength, setPasswordlengt]=useState(20);
+  const[upperCase, setUppercase]=useState(false);
+  const[lowerCase, setLowercase]=useState(false);
+  const[numbers, setNumbers]=useState(false);
+  const[symbols, setSymbols]=useState(false);
+
+  const generatePassword=(e)=>{
+    let characterList="";
+
+    if(upperCase){
+      characterList=characterList+upperCaseLetters;
+    }
+
+    if(lowerCase){
+      characterList=characterList+lowerCaseLetters;
+    }
+
+    if(numbers){
+      characterList=characterList+numbersLetters;
+    }
+
+    if(symbols){
+      characterList=characterList+symbolsLetters;
+    }
+    
+    setPassword(generatePasswordFun(characterList));
+  } 
+
+  const generatePasswordFun = (characterList) => {
+    console.log(characterList)
+    let password="";
+    const characterListLength=characterList.length;
+    console.log(characterListLength)
+    console.log(passwordLength,"--")
+
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random()*characterListLength);
+      console.log(characterIndex,"++")
+      password=password + characterList.charAt(characterIndex); 
+    }
+    console.log(password)
+    return password;
+  }
+
+
+  const copyToClipboard=()=>{
+  const texArea = document.createElement('textarea');
+  texArea.innerText=password;
+  document.body.appendChild(texArea);
+  texArea.select();
+  document.execCommand('copy');
+  texArea.remove();
+  alert("Copied");
+  }
+
+  const copyPassword =(e)=>{
+    copyToClipboard();
+    
+  }
+
   return (
     <div className="mainOuterContainer">
         <div className="mainInnerContainer">
           
           <h2 id='mainHeading'>Password Generator</h2>
           <div className='inputText'>
-          <input type="text" name="Text"  id="text"/>
-          <button type="copy" id='btnCpy'>Copy</button>
+          <input type="text" name="Text"  id="text"maxLength="20" minLength="8" defaultValue={password}/>
+          <button onClick={copyPassword} id='btnCpy'>
+            <i className='far fa-copy'></i> 
+          </button>
            
            <div className='passwordSize'>
-            <label for="length">Select Password length
-            <input id="lngth" type="text" name="text"/></label>
+            <label htmlFor="length">Select Password length
+            <input id="lngth" type="number"  name="number" max="20" min="8" defaultValue={passwordLength} onChange={(e) => setPasswordlengt(e.target.value)}/></label>
            </div>
           </div>
 
           <div className='optionList'>
-          <lable for="uppercase" className="optionsLabels">
-          <input type="checkbox" name="UpperCase" value="upperCase" id="uppercase"/>include uppercase letters
+          <lable htmlFor="uppercase" className="optionsLabels">
+          <input type="checkbox" name="UpperCase" checked={upperCase} onChange={(e) => setUppercase(e.target.checked)} id="uppercase"/>include uppercase letters
           </lable>
             <br/>
-          <lable for="lowercase" className="optionsLabels">
-          <input type="checkbox" name="LowerCase" value="lowercase" id="lowercase"/>include lowercase letters
+          <lable htmlFor="lowercase" className="optionsLabels">
+          <input type="checkbox" name="LowerCase" checked={lowerCase} onChange={(e) => setLowercase(e.target.checked)} id="lowercase"/>include lowercase letters
           </lable>
             <br/>
-          <lable for="numbers" className="optionsLabels">
-          <input type="checkbox" name="Numbers" value="numbers" id="numbers"/>include numbers
+          <lable htmlFor="numbers" className="optionsLabels">
+          <input type="checkbox" name="Numbers" checked={numbers} onChange={(e) => setNumbers(e.target.checked)} id="numbers"/>include numbers
           </lable>
             <br/>
-          <lable for="symbols" className="optionsLabels">
-          <input type="checkbox" name="Symbols" value="symbols" id="symbols"/>include symbols
+          <lable htmlFor="symbols" className="optionsLabels">
+          <input type="checkbox" name="Symbols" checked={symbols} onChange={(e) => setSymbols(e.target.checked)} id="symbols"/>include symbols
           </lable>
           </div>
           <div className='generateBtn'>
-            <button id='genBtn' type='submit'>Generate Password</button>
+            <button id='genBtn' onClick={generatePassword}>Generate Password</button>
           </div>
         </div>
     </div>
   )
 }
 
-export default Header;
+export default Generator;
